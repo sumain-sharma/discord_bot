d031351723b6c4a924c4f548b71207443951a3d7
sumain <sumain.sharma@daffodilsw.com>
2021-01-12 00:57:16 +0530

    updating .env loader

--- main.py
+++ main.py
@@ -3,15 +3,10 @@
 '''
 import os
 
-from dotenv import load_dotenv
-
 from src.bot import run_bot
 from src.data_storage import DataStorage
 
 if __name__ == "__main__":
-
-    # loading .env file
-    load_dotenv(dotenv_path='.env')
 
     # initializing DB
     with DataStorage() as data_storage_obj:
